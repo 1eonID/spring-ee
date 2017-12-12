5cd04a3baf1cdeb6cff1cbf47e62352f00a5f6e2
Leonid Pogozhev <lpogojev@gmail.com>
2017-12-12 10:58:36 +0200

    HW from Lesson 7. Part 1. Connected the database and saved doctors in PostgreSQL DB. Remove class DoctorRepository and implement JpaRepository for working with DB. Change some methods in DoctorController and DoctorService classes for correct working with JPA.

--- src/main/java/springee/doctor/DoctorConfig.java
+++ src/main/java/springee/doctor/DoctorConfig.java
@@ -1,8 +1,10 @@
 package springee.doctor;
 
 import lombok.Getter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.ArrayList;
@@ -15,4 +17,22 @@ import java.util.List;
 public class DoctorConfig {
 
     private List<String> specializations = new ArrayList<>();
+
+    @Bean
+    DoctorService doctorService(JpaDoctorRepository doctorRepository) {
+        return new DoctorService(doctorRepository);
+    }
+
+    @Bean
+    CommandLineRunner initDb(JpaDoctorRepository repository) {
+        return args -> {
+            if (!repository.findAll().isEmpty()) {
+                return;
+            }
+            repository.save(new Doctor("Robert Martin", "psychologist"));
+            repository.save(new Doctor("Josh Long", "surgeon"));
+            repository.save(new Doctor("Joshua Bloch", "ophthalmologist"));
+
+        };
+    }
 }
